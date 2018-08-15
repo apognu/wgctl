9a0372e3468d8804144ed176eece986b31e3feb7
Antoine POPINEAU <antoine.popineau@appscho.com>
2018-08-15 20:07:17 +0200

    Use wireguardctrl for netlink communication.

--- wireguard/types.go
+++ wireguard/types.go
@@ -1,11 +1,41 @@
 package wireguard
 
 import (
-	"net"
-	"time"
-
 	nl "github.com/vishvananda/netlink"
 )
+
+const (
+	NetlinkName    = "wireguard"
+	NetlinkVersion = 1
+
+	CommandGetDevice = 0
+	CommandSetDevice = 1
+
+	KeyLength = 32
+)
+
+var (
+	NetlinkFamily uint16 = 0
+)
+
+const (
+	_ = iota
+	_
+	NLWGDeviceName
+	NLWGDevicePrivateKey
+	NLWGDevicePublicKey
+	_
+	NLWGDeviceListenPort
+	NLWGDeviceFWMark
+	NLWGDevicePeers
+)
+
+var EmptyPSK = [32]byte{
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+}
 
 type WGLink struct {
 	nl.LinkAttrs
@@ -17,52 +47,4 @@ func (wg *WGLink) Type() string {
 
 func (wg *WGLink) Attrs() *nl.LinkAttrs {
 	return &wg.LinkAttrs
-}
-
-type WGPeers []*WGPeer
-type WGPeerAllowedIPs []*WGAllowedIP
-
-type WGDevice struct {
-	Name       string
-	ListenPort int
-	PublicKey  string
-	PrivateKey string
-	FWMark     int
-
-	Peers WGPeers
-}
-
-type WGPeer struct {
-	PublicKey         string
-	PresharedKey      string
-	Endpoint          *net.UDPAddr
-	AllowedIPs        WGPeerAllowedIPs
-	LastHandshake     time.Time
-	KeepaliveInterval int
-	RXBytes           int
-	TXBytes           int
-}
-
-type WGAllowedIP struct {
-	Address *net.IP
-	CIDR    int
-}
-
-type WGSockAddr4 struct {
-	AddrFamily int     `struc:"int16,little"`
-	Port       uint16  `struc:"int16,big"`
-	Addr4      []byte  `struc:"[4]byte,big"`
-	Padding    [8]byte `struc:"[8]byte"`
-}
-
-type WGSockAddr6 struct {
-	AddrFamily int     `struc:"int16,little"`
-	Port       uint16  `struc:"int16,big"`
-	Addr4      [4]byte `struc:"[4]byte"`
-	Addr6      []byte  `struc:"[16]byte,big"`
-	Padding    [4]byte `struc:"[4]byte"`
-}
-
-type WGTimestamp struct {
-	Seconds uint32 `struc:"uint32,little"`
 }
